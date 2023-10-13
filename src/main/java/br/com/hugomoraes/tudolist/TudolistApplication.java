72b66e330b10913e8680a60ea6697e55c08bbfbd
Hugo-Moraes <hugoeduardo572@gmail.com>
2023-10-12 22:08:31 -0300

    <primeiro comit>

---
+++ src/main/java/br/com/hugomoraes/tudolist/TudolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.hugomoraes.tudolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudolistApplication {
+git init
+	public static void main(String[] args) {
+		SpringApplication.run(TudolistApplication.class, args);
+	}
+
+}
